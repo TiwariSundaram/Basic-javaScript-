650e835dfe12409d393c56d05d8001a640a53605
Sundaram Tiwari <145829895+TiwariSundaram@users.noreply.github.com>
2025-03-25 13:38:12 +0000

    container config done

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,22 @@
+
+// for treat all js code as newer version use use "use strict"
+"use strict"; 
+
+
+
+// number=>2 to power 4
+// bigInt
+// String => ""
+// boolean => true/false
+// null => standalone value
+// symbol =>unique
+
+
+
+// object
+
+console.log(typeof null);
+
+
+// undefined
+console.log(typeof undefined);
