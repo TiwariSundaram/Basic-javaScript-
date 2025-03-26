5ce6b2e9ba7918dd6bb755b9422e396d97e3c0c4
Sundaram Tiwari <145829895+TiwariSundaram@users.noreply.github.com>
2025-03-26 03:55:16 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,69 @@
+// -------------------number-------------------
+
+let score = 33;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+
+// -------------------------string-------------------------
+
+let marks = "33";
+
+console.log(typeof marks);
+console.log(typeof (marks));
+
+// string => number        conversion
+let marksValue =Number(marks);
+
+console.log(typeof marksValue);
+
+
+
+
+
+
+let string = "33abc";
+let stringValue=Number(string);
+
+console.log(typeof stringValue);
+// stringValue => NaN     after converting in this type string  in  number its type is Number but value is NaN
+console.log(stringValue);
+
+
+
+
+
+// -----------------null is object----------------------- 
+let rollNo=null;
+
+console.log(typeof rollNo);
+
+let rollNoValue=Number(rollNo);
+console.log(typeof rollNoValue);
+
+// when we convert null value in number , its value 0 although type be number
+console.log(rollNoValue);
+
+
+
+
+/*  33=>33
+    "33abc" => NaN
+    true => 1,    false => 0;   
+
+*/
+
+// --------------number => boolean------------------->
+
+let number=646;
+let numberValue=Boolean(number);
+
+console.log(typeof numberValue);
+console.log(numberValue);
+
+
+ /*   1=> true  ;  0 => false
+    ""= false  ; " Sundaram" => true
+*/
