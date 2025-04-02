c1ad4956f537c181c3e82b43bccb61d05af39526
Sundaram Tiwari <145829895+TiwariSundaram@users.noreply.github.com>
2025-04-02 19:14:16 +0000

    about switch case

---
+++ 04_controlFlow/switchCase.js
@@ -0,0 +1,29 @@
+// ##############  SWITCH CASE ################
+
+// syntax =>
+    //  key = value
+
+ /*   switch (key) {
+    case value:
+        
+        break;
+   
+    default:
+        break;
+   } */
+ 
+const month = 6;
+
+switch (month) {
+    case 1:console.log("January");
+        break;
+    case 2:console.log("February");
+        break;
+    case 3:console.log("March");
+        break;
+    case 4:console.log("April");
+        break;
+    default:console.log("if all the case has not executed then i will execute");
+    
+        break;
+}
