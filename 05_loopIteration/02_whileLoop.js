7b87efd9d442f062109f874d383751d62869cd96
Sundaram Tiwari <145829895+TiwariSundaram@users.noreply.github.com>
2025-04-03 19:13:48 +0000

    about while /do-while loop

---
+++ 05_loopIteration/02_whileLoop.js
@@ -0,0 +1,35 @@
+// ###########  while loop ############
+
+// while (condition) {
+    
+// }
+
+
+let num = 0;
+
+while (num<=10) {
+    console.log(` value of num is ${num}`);
+     num = num+ 2;
+}
+
+const myArray =["batman", "shaktiman", "ironman"]
+let arr =0;
+while (arr < myArray.length) {
+    console.log(`value at index ${arr} is ${myArray[arr]}`); 
+    arr++;
+}
+
+
+// ###############  do-while loop ###########
+
+// do {
+    
+// } while (condition);
+
+
+let score = 1;
+
+do {
+   console.log(`Score is ${score}`);
+   score++;
+} while (score <=10);
